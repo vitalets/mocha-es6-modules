672ac025517161e7bd77f9970925d7a5d4dd6d6d
vitalets <vitalets@yandex-team.ru>
2017-12-04 13:39:42 +0300

    init

---
+++ test.array.js
@@ -0,0 +1,10 @@
+
+//import chai from 'chai';
+//import plus from '../../src/plus';
+// import expect from '../../src/plus.mjs';
+
+describe('array', function () {
+  it('should return length', function () {
+    chai.assert.equal(plus(1, 2), 3);
+  });
+});
